1b6c92999e501f9ef63b96cfb39d577d2738f33a
Anonymous <>
2025-02-16 08:34:14 -0800

    DRY

--- src/app/maresList.tsx
+++ src/app/maresList.tsx
@@ -37,29 +37,28 @@ export default function MaresList() {
   return <div className="flex flex-col">
     <div className="flex flex-col">
       <h1 className="text-3xl font-bold mb-4">Mare of the Day</h1>
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    >
-      {selections['mare_of_the_day']}
-    </motion.div>
+      <AnimatedMare duration={0.5}>
+        {selections['mare_of_the_day']}
+      </AnimatedMare>
       <h1 className="text-3xl font-bold mb-4">M.6. of the Week</h1>
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-    >
-      {selections['m6_of_the_week']}
-    </motion.div>
+      <AnimatedMare duration={1}>
+        {selections['m6_of_the_week']}
+      </AnimatedMare>
       <h1 className="text-3xl font-bold mb-4">Mare of Interest</h1>
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1.5 }}
-    >
-      {selections['mare_of_interest']}
-    </motion.div>
+      <AnimatedMare duration={1.5}>
+        {selections['mare_of_interest']}
+      </AnimatedMare>
     </div>
   </div>;
+}
+function AnimatedMare({ children, duration }: { children: React.ReactNode, duration: number }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration }}
+    >
+      {children}
+    </motion.div>
+  );
 }
