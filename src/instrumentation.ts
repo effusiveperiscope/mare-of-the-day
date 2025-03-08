ba1e075031653f3f36f27b0c7b7b88f7807be4b3
Anonymous <>
2025-03-07 17:33:20 -0800

    Make the instrumentation worker import dynamic to avoid accidentally loading it in edge runtime?

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,7 +1,11 @@
-import { initializeWork } from "./app/work";
+//import { initializeWork } from "./app/work";
+let initializeWork: typeof import("./app/work").initializeWork;
+
+// https://github.com/vercel/next.js/discussions/56968
 
 export async function register() {
     if (process.env.NEXT_RUNTIME === 'nodejs') {
+        initializeWork = await import("./app/work").then(m => m.initializeWork);
         initializeWork();
     }
 }
