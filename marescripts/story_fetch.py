dc9330a16a4bb7aa6e9d6e378e87491cc241ecdf
Anonymous <>
2025-03-20 15:46:46 -0700

    Do recent stories

--- marescripts/story_fetch.py
+++ marescripts/story_fetch.py
@@ -180,7 +180,7 @@ if __name__ == "__main__":
     })
 
     content = get_fimfiction_page(scraper, query=
-        'words:<2999 #mlp-fim -#anthro -#fetish -#mature -#human -#equestria-girls status:complete')
+        'words:<2999 #mlp-fim -#anthro -#fetish -#mature -#human -#equestria-girls status:complete published:week')
     
     if content:
         # Extract and process stories
