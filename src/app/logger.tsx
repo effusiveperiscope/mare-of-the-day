bb4be6fe48ee4ac9fdbf357910bd860eb93ddee4
Anonymous <>
2025-04-18 19:49:40 -0700

    appease the linter

--- src/app/logger.tsx
+++ src/app/logger.tsx
@@ -1,5 +1,5 @@
 import winston from 'winston'
-const { combine, timestamp, printf, colorize } = winston.format;
+const { combine, timestamp, printf } = winston.format;
 
 const customFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} [${level}]: ${message}`;
