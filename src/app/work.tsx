774d9d6c4c34336f60dbcf21b28d540237cc861a
Anonymous <>
2025-05-04 08:18:18 -0700

    Try catches to stop worker from crashing every single time something goes wrong

--- src/app/work.tsx
+++ src/app/work.tsx
@@ -3,6 +3,7 @@ import { rawSelections as rawMareSelections } from "./mares";
 import { rawSelections as rawEpisodeSelections } from "./episodes";
 import { writeOutDate } from "./text";
 import { generateReviews, generateStory, generateWorkout } from "./story";
+import logger from './logger';
 
 let isWorking = false;
 // Work that has to be run periodically
@@ -17,31 +18,53 @@ async function work() {
     for (const dateToRun of datesToRun) {
         const datestr = writeOutDate(dateToRun);
         // 1. Generate selection
-        if (!await getMareSelections(datestr)) {
-            const selections = rawMareSelections(dateToRun, datestr);
-            writeMareSelections(selections.mare_of_the_day, selections.m6_of_the_week, selections.mare_of_interest, datestr);
+        try {
+            if (!await getMareSelections(datestr)) {
+                const selections = rawMareSelections(dateToRun, datestr);
+                writeMareSelections(selections.mare_of_the_day, selections.m6_of_the_week, selections.mare_of_interest, datestr);
+            }
+        } catch (e) {
+            logger.error("Failed to generate mare selections", e);
         }
-        if (!await getEpisodeSelections(datestr)) {
-            const selections = await rawEpisodeSelections(dateToRun, datestr);
-            writeEpisodeSelections(selections.s1_2, selections.s3_6, selections.s7_9, datestr);
+        try {
+            if (!await getEpisodeSelections(datestr)) {
+                const selections = await rawEpisodeSelections(dateToRun, datestr);
+                writeEpisodeSelections(selections.s1_2, selections.s3_6, selections.s7_9, datestr);
+            }
+        } catch (e) {
+            logger.error("Failed to generate episode selections", e);
         }
+
         // 2. Generate story
-        if (!await getStories(datestr)) {
-            const mareSelections = await getMareSelections(datestr) as Record<string, string>;
-            const story = await generateStory([mareSelections.mare_of_the_day, mareSelections.m6_of_the_week, mareSelections.mare_of_interest,])
-            writeStory(story, datestr);
+        try {
+            if (!await getStories(datestr)) {
+                const mareSelections = await getMareSelections(datestr) as Record<string, string>;
+                const story = await generateStory([mareSelections.mare_of_the_day, mareSelections.m6_of_the_week, mareSelections.mare_of_interest,])
+                writeStory(story, datestr);
+            }
+        } catch (e) {
+            logger.error("Failed to generate story", e);
         }
         // 3. Generate reviews
-        if ((await getReviews(datestr)).length === 0) {
-            const reviews = await generateReviews();
-            for (const review of reviews) {
-                writeReview(review.author, review.title, review.review, review.url, datestr);
+        try {
+            if ((await getReviews(datestr)).length === 0) {
+                const reviews = await generateReviews();
+                for (const review of reviews) {
+                    writeReview(review.author, review.title, review.review, review.url, datestr);
+                }
             }
+        } catch (e) {
+            logger.error("Failed to generate reviews", e);
         }
+
         // 4. Generate workouts
-        if (!await getWorkouts(datestr)) {
-            const workout = await generateWorkout();
-            writeWorkout(workout, datestr);
+        try {
+            if (!await getWorkouts(datestr)) {
+                const workout = await generateWorkout();
+                writeWorkout(workout, datestr);
+            }
+        } catch (e) {
+            logger.error("Failed to generate workout", e);
         }
         // Hm... we're violating DRY...
     }
